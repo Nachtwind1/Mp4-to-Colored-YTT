c2801549186ecc1dcc4e6b4010c5ff514707ea8c
Nachtwind1 <124518219+Nachtwind1@users.noreply.github.com>
2025-02-14 16:32:14 +0100

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,1 +1,6 @@
-python3 main.py --file "./Crash_Youtube V2.mov" --collums 640 --coloraccuracy 1 --scale 0.5
+echo "put your Input.mp4 in this folder"
+read -p "Press Enter to continue"
+
+python3 main.py --file "./input.mp4" -collums 64 --coloraccuracy 2 --scale 0.5
+
+echo "that was it, you should have a .YTT file in the output folder"
