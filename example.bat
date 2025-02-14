4c863736653e550f367a54b1408e51e2e256f6d8
Nachtwind1 <124518219+Nachtwind1@users.noreply.github.com>
2025-02-14 16:31:47 +0100

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
-echo 'put your Input.mp4 in this folder and then just press enter on this console'
+echo put your Input.mp4 in this folder and then just press enter on this console
 
 pause
 @ECHO ON
 python main.py --file "Input.mp4" --collums 64 --coloraccuracy 2 --scale 0.5
 
 @ECHO OFF
-echo "that was it, you should have a .YTT file in the output folder"
+echo that was it, you should have a .YTT file in the output folder
 pause
